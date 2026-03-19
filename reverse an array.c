2c32dd5d922e12a779ca170590cee913b34a35ca
shall-bal <mohdahshamarshan@gmail.com>
2026-03-18 23:24:55 -0700

    Create reverse an array.c

---
+++ reverse an array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[10];
+
+    printf("Enter 10 numbers:\n");
+    for(int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Numbers in reverse order:\n");
+    for(int i = 9; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
